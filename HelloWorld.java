18f0a9bdf80cb4cf14839751940deb800c46ab32
TheNotary <no@mail.plz>
2015-04-25 19:37:32 -0500

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// To compile, do javac HelloWorld.java
+// Then to run do java HelloWorld
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!");
+	}
+}
